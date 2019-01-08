cb72c78e013851f4a0bb5a0209480a4f957955d7
Gyanendra Sonkar <gyanendra.sonkar@factset.com>
2019-01-08 12:59:28 +0530

    Module 1 Creating the APi and Returning Resources
    CitiesDataStore :To store the data about the cites.
    CitiesController contains the action methods for Geting Cities and releated data
    PointsOfInterestController:contains the get method for returning all point of interest of a city or specific point of interest of the city
    
    Model Class :CityDto.cs ,PointOfInterestDto.cs

---
+++ CityInfo.API/CityInfo.API/Controller/CitiesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controller
+{
+    [Route("api/cities")]
+    public class CitiesController :Microsoft.AspNetCore.Mvc.Controller
+    {
+        //The action method should not static : http://www.tutorialsteacher.com/mvc/action-method-in-mvc
+
+        //[HttpGet("api/cities")]
+        //public JsonResult GetCities()
+        //{
+        //    return new JsonResult(new List<object>
+        //    {
+        //        new {id=1,name="New York City"},
+        //        new {id=2,name="Boston"}
+        //    });
+        //}
+
+        //Reading Values using Model
+        
+        public  IActionResult GetCitiesUsingModel()
+        {
+           var temp=new JsonResult(CitiesDataStore.Current.Cities);
+            return Ok(temp);
+
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+             return Ok(city);
+            return new JsonResult(city);
+        }
+    }
+}
---
+++ CityInfo.API/CityInfo.API/Controller/PointsOfInterestController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controller
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController :Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{pointOfInterestId}")]
+        public IActionResult GetPointsOfInterest(int cityId,int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (pointOfInterest == null)
+                return NotFound();
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
